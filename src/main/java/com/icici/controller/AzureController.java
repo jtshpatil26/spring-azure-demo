07792cb5f8281fc5c0b939c59c61b7c6a7934653
jtshpatil26 <jtshpatil26@gmail.com>
2021-08-21 16:05:22 +0530

    initial commit

---
+++ src/main/java/com/icici/controller/AzureController.java
@@ -0,0 +1,14 @@
+package com.icici.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureController {
+
+	@GetMapping("/testAzure")
+	public String test() {
+		return "Welcome to Azure Demo";
+	}
+	
+}
